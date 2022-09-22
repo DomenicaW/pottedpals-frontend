ad6172c2411b7d555a712cfc227bba06f9058d46
dwallace <domenica.wallace5@gmail.com>
2022-09-21 22:36:08 -0400

    added hello world

--- potted-pals-proj3/src/App.js
+++ potted-pals-proj3/src/App.js
@@ -1,25 +1,12 @@
+// Bring in React instance from React
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Define our Hello component
+function Hello() {
+  // Make sure to return some UI
+  return <h1>Hello World</h1>;
 }
 
-export default App;
+export default Hello;
