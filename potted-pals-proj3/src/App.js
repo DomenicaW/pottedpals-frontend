82441e84404cd36bf402cbd89bf78182c30f72cd
dwallace <domenica.wallace5@gmail.com>
2022-09-21 22:42:00 -0400

    added my test message

--- potted-pals-proj3/src/App.js
+++ potted-pals-proj3/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 // Define our Hello component
 function Hello() {
   // Make sure to return some UI
-  return <h1>Hello World</h1>;
+  return <h1>Hello Everyoneeeeee</h1>;
 }
 
 export default Hello;
