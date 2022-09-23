4ba33ee5deef28fd9014078bfa69011a72300c11
amregalado <regalado.adriana@gmail.com>
2022-09-23 00:13:12 -0400

    Deleted uneeded files, created mock-images folder for styling

--- potted-pals-proj3/src/index.js
+++ potted-pals-proj3/src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 // STYLING
 import "./css-sheets/index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
