a0a3b5ff8a41c567786790d9fdc4dad0765305cd
amregalado <regalado.adriana@gmail.com>
2022-09-26 21:55:06 -0400

    Added target blank attribute to link on contact page

--- potted-pals-proj3/src/components/ContactCard.js
+++ potted-pals-proj3/src/components/ContactCard.js
@@ -13,26 +13,24 @@ function ContactCard(props) {
   return (
     <div className="ContactDiv">
       <div className="ImgCircle">
-      <a href={props.link}>
-      <img
+        <a href={props.link} target="_blank">
+          <img
             src={`${process.env.PUBLIC_URL}mock-images/Asset-person.png`}
-          alt="avatar"
-        />
-      </a>
-      
+            alt="avatar"
+          />
+        </a>
       </div>
 
       <div className="ContactText">
         <h3 className="ContactName"> {props.name} </h3>
         <h3 className="ContactCardTitle"> {props.title} </h3>
         <h3 className="">{props.links}</h3>
-        <a href={props.link}>
-        <img
-          src={`${process.env.PUBLIC_URL}mock-images/${props.image}`}
-          alt="linkedin-logo"
-        />
+        <a href={props.link} target="_blank">
+          <img
+            src={`${process.env.PUBLIC_URL}mock-images/${props.image}`}
+            alt="linkedin-logo"
+          />
         </a>
-     
       </div>
     </div>
   );
