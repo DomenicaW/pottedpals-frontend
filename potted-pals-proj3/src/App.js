0086bfbb9847fd145ecf2fe57914114e5f5b4de3
amregalado <regalado.adriana@gmail.com>
2022-09-28 21:30:59 -0400

    Used fetch to make a database call - working - data displaying in console log

--- potted-pals-proj3/src/App.js
+++ potted-pals-proj3/src/App.js
@@ -1,5 +1,5 @@
-import { Outlet } from "react-router-dom";
 import React, { Component } from "react";
+import { Outlet } from "react-router-dom";
 
 //  Keeping the below in to remind of Link import in case there are issues in future
 //  import { Outlet, Link } from "react-router-dom";
@@ -20,10 +20,36 @@ if (process.env.NODE_ENV === "development") {
 }
 console.log("current base url:", baseURL);
 
-
-
 // CLASS BASED COMPONENT
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      plants: [],
+    };
+  }
+
+  // This is a method because it is associated with a class
+  getPlants = () => {
+    fetch(baseURL + "/plants")
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return [];
+        }
+      })
+      .then(data => {
+        console.log("data:", data);
+        this.setState({ plants: data.plants });
+      });
+  };
+
+  // componentDidMount
+  componentDidMount() {
+    this.getPlants();
+  }
+
   render() {
     return (
       <div className="App">
