8e64513a210f14841f6068dbedce7fceae9df650
dwallace <domenica.wallace5@gmail.com>
2022-10-04 01:07:43 -0400

    built constructor & click function to console log yt video object

--- potted-pals-proj3/src/components/Youtube.js
+++ potted-pals-proj3/src/components/Youtube.js
@@ -4,15 +4,33 @@ const API = 'AIzaSyCEXxjr-ZNVAWdnx0AKpUia1qoPxweqSPA';
 const channelId = 'UC9afZE1IVR6JhOjKXiEddIA';
 const result = 3
 
-https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC9afZE1IVR6JhOjKXiEddIA&key=AIzaSyCEXxjr-ZNVAWdnx0AKpUia1qoPxweqSPA&maxResults=3
-
+//string interpolation from video object
 let musicURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId${channelId}&key=${API}&maxResults=${result}`
 
 class Youtube extends Component {
+
+    constructor(props){
+      super(props);
+
+      this.state = {
+        ytmusic: []
+      };
+      this.clicked = this.clicked.bind(this);
+    }
+    clicked(){
+      // console.log('button was clicked');
+      fetch(musicURL)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+      })
+    }
+
     render(){
+      // console.log(musicURL)
       return(
         <div className ='youtube'>
-        <button>Click for Calm</button>
+        <button onClick={this.clicked}>Click for Calm</button>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/fASV9M32u3k" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 
         </div>
